c4005ab3015bddc1b0f756adbddb5106fdf56b2d
rahulraut11 <rahulraut110506@gmail.com>
2025-10-08 02:22:37 +0530

    added soln

--- Sliding_window/p4.cpp
+++ Sliding_window/p4.cpp
@@ -5,11 +5,40 @@ using vi = vector<ll>;
 
 int main()
 {
-    ll t;
-    cin >> t;
-    while (t--)
+    int n, k;
+    cin >> n >> k;
+    vi v(n);
+    ll x, a, b, c;
+    cin >> x >> a >> b >> c;
+    v[0] = x;
+    for (int i = 1; i < n; i++)
     {
-        
+        v[i] = (1LL * a * v[i - 1] + b) % c;
     }
+    vi pref(n, 0);
+    vi suf(n + 1, 0);
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % k == 0)
+            pref[i] = v[i];
+        else
+            pref[i] = (pref[i - 1] | v[i]);
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (i % k == k - 1)
+            suf[i] = v[i];
+        else
+            suf[i] = (suf[i + 1] | v[i]);
+    }
+
+    for (int i = 0; i <= n - k; i++)
+    {
+        int l = i, r = i + k - 1;
+        int cur_or = (pref[r] | suf[l]);
+        res ^= cur_or;
+    }
+    cout << res << endl;
     return 0;
 }
--- Sliding_window/p5.cpp
+++ Sliding_window/p5.cpp
@@ -5,11 +5,33 @@ using vi = vector<ll>;
 
 int main()
 {
-    ll t;
-    cin >> t;
-    while (t--)
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    unordered_map<int, int> mp;
+    for (int i = 0; i < k; i++)
     {
-        
+        mp[a[i]]++;
     }
+
+    vi res(n - k + 1, 0);
+    res[0] = mp.size();
+
+    for (int i = k; i < n; i++)
+    {
+        mp[a[i - k]]--;
+        if (mp[a[i - k]] == 0)
+            mp.erase(a[i - k]);
+        mp[a[i]]++;
+        res[i - k + 1] = mp.size();
+    }
+    for (int i = 0; i <= n - k; i++)
+    {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
