f2de52ddd64691878de4441c8730d43324736608
rahulraut11 <rahulraut110506@gmail.com>
2025-10-08 01:41:49 +0530

    added soln

--- Sliding_window/p3.cpp
+++ Sliding_window/p3.cpp
@@ -5,11 +5,27 @@ using vi = vector<ll>;
 
 int main()
 {
-    ll t;
-    cin >> t;
-    while (t--)
+    int n, k;
+    cin >> n >> k;
+    vi v(n);
+    ll x, a, b, c;
+    cin >> x >> a >> b >> c;
+    v[0] = x;
+    for (int i = 1; i < n; i++)
     {
-        
+        v[i] = (1LL * a * v[i - 1] + b) % c;
     }
+    ll res = 0;
+    for (int i = 0; i < k; i++)
+    {
+        res ^= v[i];
+    }
+    int sum = res;
+    for (int i = k; i < n; i++)
+    {
+        sum = sum ^ v[i - k] ^ v[i];
+        res ^= sum;
+    }
+    cout << res << endl;
     return 0;
 }
