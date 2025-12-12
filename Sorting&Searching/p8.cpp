818a1a487f4fd217301f06d57f632515a1200716
rahulraut11 <rahulraut110506@gmail.com>
2025-12-13 00:20:39 +0530

    added soln

--- Sorting&Searching/p8.cpp
+++ Sorting&Searching/p8.cpp
@@ -1,17 +1,25 @@
+// Maximum Subarray Sum
 #include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
-using vi = vector < ll > ;
+using vi = vector<ll>;
 #define pb(i) push_back(i)
-#define all(x)(x).begin(), (x).end()
+#define all(x) (x).begin(), (x).end()
 
 signed main()
 {
-    ll t;
-    cin >> t;
-    while (t--) {
-       
+    ll n;
+    cin >> n;
+    vi a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    ll ans = a[0], end = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        end = max(a[i], end + a[i]);
+        ans = max(ans, end);
     }
+    cout << ans << endl;
     return 0;
 }
