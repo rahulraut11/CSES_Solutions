30d803dd8ce94212c147c7e09341d4c89be049df
rahulraut11 <rahulraut110506@gmail.com>
2025-12-13 01:46:34 +0530

    added soln

--- Sorting&Searching/p11.cpp
+++ Sorting&Searching/p11.cpp
@@ -1,4 +1,4 @@
-//
+// Collecting  Numbers
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -11,7 +11,17 @@ signed main()
 {
     ll n;
     cin >> n;
-    vi a(n);
+    vi pos(n + 1), a(n);
     for (int i = 0; i < n; i++)
+    {
         cin >> a[i];
+        pos[a[i]] = i;
+    }
+    ll ans = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (pos[i] > pos[i + 1])
+            ans++;
+    }
+    cout << ans << endl;
 }
